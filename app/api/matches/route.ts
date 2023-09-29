0c264ca016c90673b7a3b47e2ffbe34b1aff82e7
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-09-28 23:55:18 -0700

    fine no search params

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -3,15 +3,9 @@ import clientPromise from "@/lib/mongodb";
 
 export async function GET(request: NextRequest) {
   try {
-    const searchParams = request.nextUrl.searchParams;
-    console.log("SearchParams: ", searchParams);
-    const ranked = searchParams.get("ranked");
     const client = await clientPromise;
     const db = client.db("ShadowrunWeb");
-    const matches = await db
-      .collection("Matches")
-      .find({ ranked: ranked === "true" })
-      .toArray();
+    const matches = await db.collection("Matches").find().toArray();
     return NextResponse.json({
       ok: true,
       matches: matches,
