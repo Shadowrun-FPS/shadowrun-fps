8c65a5b4d5b86951a402e5c2a09b8f5e6b35e6bf
Johnathan <johnathancarlton@outlook.com>
2023-11-18 23:19:29 -0800

    Added Latest Posts to Intro

--- app/docs/installation/page.tsx
+++ app/docs/installation/page.tsx
@@ -1,221 +1,226 @@
 import Link from "next/link";
 import Image from "next/image";
+import { DocHero } from "@/components/doc-hero";
 
 export default function InstallationPage() {
   return (
-    <div
-      className="mt-2 ml-14 lg:prose-xl dark:prose-invert outline"
-      style={{
-        backgroundImage: 'url("/shadowrun_invite_banner.png")', // <-- Provide the correct path
-        backgroundSize: "cover",
-        backgroundAttachment: "fixed",
-        backgroundPosition: "right",
-        width: "40%",
-      }}
-    >
-      <div className="ml-16 prose lg:prose-xl dark:prose-invert">
-        <br />
-        <h1>Installation Guide</h1>
-        <article>
-          <h2>Preface</h2>
+    <>
+      <DocHero title={""} />
+      <div
+        className="mt-2 ml-14 lg:prose-xl dark:prose-invert"
+        // style={{
+        //   backgroundImage: 'url("/shadowrun_invite_banner.png")', // <-- Provide the correct path
+        //   backgroundSize: "cover",
+        //   backgroundAttachment: "fixed",
+        //   backgroundPosition: "right",
+        //   width: "40%",
+        // }}
+      >
+        <div className="ml-16 prose lg:prose-xl dark:prose-invert">
+          <br />
+          <h1>Installation Guide</h1>
+          <article>
+            <h2>Preface</h2>
+            <p>
+              Trouble Installing? Please use the following channels if you are
+              having issues installing:
+            </p>
+            <ul>
+              <li>
+                <Link href="/docs/troubleshoot">General Errors and Info</Link>
+              </li>
+              <li>
+                <Link
+                  target="_blank"
+                  href="https://discord.com/channels/930362820627943495/938788090087088168"
+                >
+                  Troubleshoot Chat
+                </Link>
+              </li>
+              <li>
+                <Link href="/docs/support">Support Ticket</Link>
+              </li>
+            </ul>
+          </article>
+          <h2 className="mt-16 mb-0 text-3xl font-bold not-prose">
+            File Archiver
+          </h2>
+          <p className="mt-0 mb-0">
+            In order to extract any .7z file, download and install:{" "}
+            <Link href="https://www.7-zip.org/a/7z2201-x64.exe">7-Zip</Link> or{" "}
+            <Link href="https://github.com/peazip/PeaZip/releases/download/8.9.0/peazip-8.9.0.WIN64.exe">
+              Peazip
+            </Link>
+          </p>
+          <p className="mt-0 not-prose">
+            <b>
+              <em>Extracting with Winrar is known to cause issues.</em>
+            </b>
+          </p>
+          <h2 className="mt-16 mb-0 text-3xl font-bold not-prose">
+            FPS Limitations
+          </h2>
+          <p className="mt-o not-prose">
+            Parts of the game may malfunction if the fps is not limited. See the
+            Limit FPS section under In Game Preferences below.
+          </p>
+          <br />
+          <h2>Free Installation Guide</h2>
+          <h2 className="mt-8 mb-0 text-3xl font-bold not-prose">Step 1</h2>
           <p>
-            Trouble Installing? Please use the following channels if you are
-            having issues installing:
+            Download & Install:{" "}
+            <Link href="https://community.pcgamingwiki.com/files/file/1012-microsoft-games-for-windows-live/?do=download&r=3736&confirm=1&t=1&csrfKey=72a35fbfd8ae582fe891f867e376ddcc">
+              Games For Windows Live
+            </Link>
+          </p>
+          <ul>
+            <li>Extract the folder</li>
+            <li>
+              Run <u>gfwllivesetup.exe</u>
+            </li>
+            <li>
+              <em>
+                When prompted, select Launch and the GFWL program will launch
+                showing Connection Error. This is to be expected and can be
+                ignored. You DO NOT need to launch this program after
+                installing!
+              </em>
+            </li>
+          </ul>
+          <h3>Step 2:</h3>
+          <p>
+            Download{" "}
+            <Link
+              href="https://mega.nz/file/5LdjgJQY#XMIClDPN0j0p7FrjNTGL3518OU3nrJl-xCA5W5jZZcg"
+              target="_blank"
+            >
+              Shadowrun - DXVK (2.3).zip
+            </Link>{" "}
+            version of the Pre-installed and updated game.
           </p>
           <ul>
             <li>
-              <Link href="/docs/troubleshoot">General Errors and Info</Link>
-            </li>
-            <li>
-              <Link
-                target="_blank"
-                href="https://discord.com/channels/930362820627943495/938788090087088168"
-              >
-                Troubleshoot Chat
-              </Link>
-            </li>
-            <li>
-              <Link href="/docs/support">Support Ticket</Link>
+              Open Shadowrun DXVK (2.3).zip and drag and drop or extract the
+              Shadowrun folder contained inside.
             </li>
           </ul>
-        </article>
-        <h2 className="mt-16 mb-0 text-3xl font-bold not-prose">
-          File Archiver
-        </h2>
-        <p className="mt-0 mb-0">
-          In order to extract any .7z file, download and install:{" "}
-          <Link href="https://www.7-zip.org/a/7z2201-x64.exe">7-Zip</Link> or{" "}
-          <Link href="https://github.com/peazip/PeaZip/releases/download/8.9.0/peazip-8.9.0.WIN64.exe">
-            Peazip
-          </Link>
-        </p>
-        <p className="mt-0 not-prose">
-          <b>
-            <em>Extracting with Winrar is known to cause issues.</em>
-          </b>
-        </p>
-        <h2 className="mt-16 mb-0 text-3xl font-bold not-prose">
-          FPS Limitations
-        </h2>
-        <p className="mt-o not-prose">
-          Parts of the game may malfunction if the fps is not limited. See the
-          Limit FPS section under In Game Preferences below.
-        </p>
-        <br />
-        <h2>Free Installation Guide</h2>
-        <h2 className="mt-8 mb-0 text-3xl font-bold not-prose">Step 1</h2>
-        <p>
-          Download & Install:{" "}
-          <Link href="https://community.pcgamingwiki.com/files/file/1012-microsoft-games-for-windows-live/?do=download&r=3736&confirm=1&t=1&csrfKey=72a35fbfd8ae582fe891f867e376ddcc">
-            Games For Windows Live
-          </Link>
-        </p>
-        <ul>
-          <li>Extract the folder</li>
-          <li>
-            Run <u>gfwllivesetup.exe</u>
-          </li>
-          <li>
-            <em>
-              When prompted, select Launch and the GFWL program will launch
-              showing Connection Error. This is to be expected and can be
-              ignored. You DO NOT need to launch this program after installing!
-            </em>
-          </li>
-        </ul>
-        <h3>Step 2:</h3>
-        <p>
-          Download{" "}
-          <Link
-            href="https://mega.nz/file/5LdjgJQY#XMIClDPN0j0p7FrjNTGL3518OU3nrJl-xCA5W5jZZcg"
-            target="_blank"
-          >
-            Shadowrun - DXVK (2.3).zip
-          </Link>{" "}
-          version of the Pre-installed and updated game.
-        </p>
-        <ul>
-          <li>
-            Open Shadowrun DXVK (2.3).zip and drag and drop or extract the
-            Shadowrun folder contained inside.
-          </li>
-        </ul>
-        <h3>Step 3:</h3>
-        <p>Launch Shadowrun.exe</p>
-        <ul>
-          <li>
-            Then sign in to your Microsoft account by pressing home or fn+home
-            on the keyboard. (This is any account that can log in to{" "}
-            <Link target="_blank" href="https://www.xbox.com/">
-              Xbox.com
-            </Link>
-            )
-          </li>
-        </ul>
-        <ul>
-          <li>
-            Enter your key when prompted. The first time login/activation
-            loading circle may take 5-20 minutes (see "Getting a key" section
-            below)
-          </li>
-        </ul>
-        <h3>Step 4:</h3>
-        <p>Configure Resolution and In-game advanced video settings.</p>
-        <ul>
-          <li>
-            While the other settings are up to your preferences of detail,
-            quality, and performance, it is important to set Vertical Sync to
-            Disabled in the in-game's Advanced Video settings.
-          </li>
-        </ul>
-        <Image
-          src="/shadowrunvsync.png"
-          alt="Installation Guide Image"
-          width={500}
-          height={500}
-        />
-        <br />
-        <div>
-          <h2>Getting a key</h2>
-          <p>
-            If you do not have an old GFWL key laying around, you can purchase{" "}
-            <Link
-              target="_blank"
-              href="https://store.steampowered.com/app/15620/Warhammer_40000_Dawn_of_War_II/"
-            >
-              Warhammer 40,000: Dawn of War II
-            </Link>{" "}
-            from steam to use its GFWL key to activate Shadowrun.
-          </p>{" "}
-          <p>
-            {" "}
-            Or if you already own{" "}
-            <Link
-              target="_blank"
-              href="https://store.steampowered.com/app/12360/FlatOut_Ultimate_Carnage/"
-            >
-              Flatout Ultimate Carnage
-            </Link>{" "}
-            on Steam, you can use its CD key to activate Shadowrun as well.
-          </p>
-          <p>
-            This is possible because specific GFWL games can cross authenticate
-            each other - see{" "}
-            <Link href="/docs/troubleshoot">General Errors and Info</Link>
-          </p>
+          <h3>Step 3:</h3>
+          <p>Launch Shadowrun.exe</p>
+          <ul>
+            <li>
+              Then sign in to your Microsoft account by pressing home or fn+home
+              on the keyboard. (This is any account that can log in to{" "}
+              <Link target="_blank" href="https://www.xbox.com/">
+                Xbox.com
+              </Link>
+              )
+            </li>
+          </ul>
+          <ul>
+            <li>
+              Enter your key when prompted. The first time login/activation
+              loading circle may take 5-20 minutes (see "Getting a key" section
+              below)
+            </li>
+          </ul>
+          <h3>Step 4:</h3>
+          <p>Configure Resolution and In-game advanced video settings.</p>
+          <ul>
+            <li>
+              While the other settings are up to your preferences of detail,
+              quality, and performance, it is important to set Vertical Sync to
+              Disabled in the in-game's Advanced Video settings.
+            </li>
+          </ul>
           <Image
-            src="/dawnofwar2.jpg"
+            src="/shadowrunvsync.png"
             alt="Installation Guide Image"
             width={500}
-            height={300}
+            height={500}
           />
-          <p>
-            <strong>Once Purchased</strong>
-          </p>
-          <ul>
-            <li>No installation is required</li>
-            <li>
-              In your Steam Library, right-click on your newly purchased game
-            </li>
-            <li>Go to manage - CD keys</li>
-            <li>Copy the Legacy GFWL key</li>
-          </ul>
-          <p>
-            <strong>How many uses can I get out of a key?</strong>
-          </p>
-          <p>
-            It is theorized that each individual key can be used between 10-15
-            times with no ties to account or computer. This means you can share
-            your key with anyone until the key is permanently used up.
-          </p>
-          <p>
-            For further key details, see{" "}
-            <Link
-              target="_blank"
-              href="https://discord.com/channels/930362820627943495/1106078455226957854/1106080395352604723"
-            >
-              PC Key Activation
-            </Link>
-          </p>
           <br />
-        </div>
-        <div>
-          <h2>
-            <strong>Sharing Keys</strong>
-          </h2>
-          <p>
-            A channel for sharing keys. You can give a shout if you notice a key
-            has been all used up. Feel free to leave one!{" "}
-            <Link
-              target="_blank"
-              href="https://discord.com/channels/930362820627943495/1130511102938194063"
-            >
-              Public Key Share
-            </Link>{" "}
-          </p>
-          <br />
+          <div>
+            <h2>Getting a key</h2>
+            <p>
+              If you do not have an old GFWL key laying around, you can purchase{" "}
+              <Link
+                target="_blank"
+                href="https://store.steampowered.com/app/15620/Warhammer_40000_Dawn_of_War_II/"
+              >
+                Warhammer 40,000: Dawn of War II
+              </Link>{" "}
+              from steam to use its GFWL key to activate Shadowrun.
+            </p>{" "}
+            <p>
+              {" "}
+              Or if you already own{" "}
+              <Link
+                target="_blank"
+                href="https://store.steampowered.com/app/12360/FlatOut_Ultimate_Carnage/"
+              >
+                Flatout Ultimate Carnage
+              </Link>{" "}
+              on Steam, you can use its CD key to activate Shadowrun as well.
+            </p>
+            <p>
+              This is possible because specific GFWL games can cross
+              authenticate each other - see{" "}
+              <Link href="/docs/troubleshoot">General Errors and Info</Link>
+            </p>
+            <Image
+              src="/dawnofwar2.jpg"
+              alt="Installation Guide Image"
+              width={500}
+              height={300}
+            />
+            <p>
+              <strong>Once Purchased</strong>
+            </p>
+            <ul>
+              <li>No installation is required</li>
+              <li>
+                In your Steam Library, right-click on your newly purchased game
+              </li>
+              <li>Go to manage - CD keys</li>
+              <li>Copy the Legacy GFWL key</li>
+            </ul>
+            <p>
+              <strong>How many uses can I get out of a key?</strong>
+            </p>
+            <p>
+              It is theorized that each individual key can be used between 10-15
+              times with no ties to account or computer. This means you can
+              share your key with anyone until the key is permanently used up.
+            </p>
+            <p>
+              For further key details, see{" "}
+              <Link
+                target="_blank"
+                href="https://discord.com/channels/930362820627943495/1106078455226957854/1106080395352604723"
+              >
+                PC Key Activation
+              </Link>
+            </p>
+            <br />
+          </div>
+          <div>
+            <h2>
+              <strong>Sharing Keys</strong>
+            </h2>
+            <p>
+              A channel for sharing keys. You can give a shout if you notice a
+              key has been all used up. Feel free to leave one!{" "}
+              <Link
+                target="_blank"
+                href="https://discord.com/channels/930362820627943495/1130511102938194063"
+              >
+                Public Key Share
+              </Link>{" "}
+            </p>
+            <br />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
--- app/docs/introduction/page.tsx
+++ app/docs/introduction/page.tsx
@@ -1,58 +1,104 @@
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { DocHero } from "@/components/doc-hero";
 
 export default function IntroductionPage() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="prose text-center lg:prose-xl dark:prose-invert">
-        <h1>Introduction Page is Under Construction</h1>
-        <p>
-          Were currently working hard to enhance your experience. Our website is
-          undergoing some renovations to bring you exciting new content and
-          features.
-        </p>
-        <p>
-          We appreciate your patience and cant wait to share the improvements
-          with you. Please check back soon!
-        </p>
-      </div>
+    <>
+      <DocHero title={""} />
+      <div className="flex flex-col items-center justify-center">
+        <div className="prose text-center lg:prose-xl dark:prose-invert">
+          <br />
+          <h1>Introduction Page is Under Construction</h1>
+          <p>
+            Were currently working hard to enhance your experience. Our website
+            is undergoing some renovations to bring you exciting new content and
+            features.
+          </p>
+          <p>
+            We appreciate your patience and cant wait to share the improvements
+            with you. Please check back soon!
+          </p>
+        </div>
 
-      <div className="mt-8 text-center">
-        <h2>Contact Us</h2>
-        <p>
-          If you have urgent inquiries or need assistance, feel free to reach
-          out to us through the community discord.
-        </p>
-        <ul>
-          {/* <li>
+        <div className="mt-8 text-center">
+          <h2>Contact Us</h2>
+          <p>
+            If you have urgent inquiries or need assistance, feel free to reach
+            out to us through the community discord.
+          </p>
+          <ul>
+            {/* <li>
             Email:{" "}
             <a href="mailto:3M3RGx@protonmail.com">info@ShadowrunFPS.com</a>
           </li> */}
-          {/* <li>
+            {/* <li>
             Phone: <a href="tel:+1234567890">+1 (234) 567-890</a>
           </li> */}
-        </ul>
-      </div>
-
-      <div className="mt-8 text-center">
-        <h2>Stay Connected</h2>
-        <p>
-          Follow us on social media for updates and announcements about our
-          progress:
-        </p>
-        <ul>
+          </ul>
+        </div>
+        <br />
+        <section
+          id="blog"
+          className="grid grid-cols-1 p-8 mx-auto prose rounded max-w-screen-2xl bg-muted dark:prose-invert "
+        >
+          <h2 className="text-6xl text-bold ">Latest Community Posts</h2>
           <br />
-          <li>
-            <a
-              href="https://twitter.com/i/communities/1688188047064470011"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Twitter
-            </a>
-          </li>
-          {/* <li>
+          <div id="latest-posts-list" style={{ display: "flex", gap: "24px" }}>
+            <div id="post">
+              <Image
+                src="/2023NashvilleLAN.jpeg"
+                alt=""
+                width={800}
+                height={800}
+                object-fit="cover"
+              ></Image>
+
+              <h3 className="text-2xl">Shadowrun 2023 LAN - Nashville, TN</h3>
+              <div className="text-lg">
+                How 21 friends took over an AirBnB in Nashville, TN for
+                Shadowrun's first LAN event after many years in 2017. Equiped
+                with eight PCs and a shared passion, one great weekend ensued.
+              </div>
+            </div>
+            <div id="post" className="">
+              <Image
+                src="/Shadowrun2017AnaheimLAN.jpeg"
+                alt=""
+                width={800}
+                height={800}
+                object-fit="cover"
+              ></Image>
+
+              <h3 className="text-2xl">Shadowrun 2017 LAN - Anaheim, CA</h3>
+              <div className="text-lg">
+                How 13 friends took over an Esports Arena in Anaheim, CA for
+                Shadowrun's first LAN event after many years in 2017. Equiped
+                with Xbox 360s and a shared passion, one great weekend ensued.
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div className="mt-8 text-center">
+          <h2>Stay Connected</h2>
+          <p>
+            Follow us on social media for updates and announcements about our
+            progress:
+          </p>
+          <ul>
+            <br />
+            <li>
+              <a
+                href="https://twitter.com/i/communities/1688188047064470011"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </li>
+            {/* <li>
             <a
               href="https://www.facebook.com/example"
               target="_blank"
@@ -61,7 +107,7 @@ export default function IntroductionPage() {
               Facebook
             </a>
           </li> */}
-          {/* <li>
+            {/* <li>
             <a
               href="https://www.instagram.com/example"
               target="_blank"
@@ -70,8 +116,9 @@ export default function IntroductionPage() {
               Instagram
             </a>
           </li> */}
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
