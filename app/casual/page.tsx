fe4bc687ba845a3f2694a303862bd27cb61c5f4c
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-11-26 14:20:26 -0800

    coming soon pages all complete for every route

---
+++ app/casual/page.tsx
@@ -0,0 +1,12 @@
+import ComingSoon from "../coming-soon";
+
+const CasualPage: React.FC = () => {
+  return (
+    <ComingSoon
+      title={"Casual pick up games"}
+      description="Will support lots of game modes and types."
+    />
+  );
+};
+
+export default CasualPage;
