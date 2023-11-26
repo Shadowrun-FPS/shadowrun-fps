dcb390bca5b56bd8559b75c33ce5041571a42bd1
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-11-26 14:05:10 -0800

    not found page fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,14 +4,20 @@ import { Button } from "@/components/ui/button";
 
 export default function NotFoundPage() {
   return (
-    <div className="flex flex-col items-center justify-center p-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center p-4 bg-muted">
       <div className="w-full max-w-md text-center">
-        <div className="mb-4">
-          <Image src="/404.png" alt="Page Not Found" width={300} height={300} />
+        <div className="flex justify-center mb-4">
+          <Image
+            src="/404-image.png"
+            alt="Page Not Found"
+            width={500}
+            height={500}
+          />
         </div>
-        <h1 className="mb-4 font-bold text-gray-900 text-8xl">404</h1>
-        <p className="mb-6 text-2xl text-gray-700">Page Not Found</p>
-        <Button variant="outline">
+        <h3 className="mb-6 text-3xl prose dark:prose-invert">
+          Page Not Found
+        </h3>
+        <Button variant="default">
           <Link href="/">Return Home</Link>
         </Button>
       </div>
