c82267e8088667539ea8ca568736ab770010da0a
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-11-27 19:24:02 -0800

    header nav bar alignment

--- components/header.tsx
+++ components/header.tsx
@@ -6,11 +6,11 @@ import DiscordSignIn from "./discord/discord-sign-in";
 function Header() {
   return (
     <header className="flex p-6 lg:px-8 bg-background">
-      <MainLogo className="flex-1" />
-      <nav className="flex justify-center flex-1">
+      <MainLogo />
+      <nav className="flex flex-1">
         <MainNavMenu />
       </nav>
-      <div className="flex justify-end flex-1 gap-4">
+      <div className="flex justify-end gap-4">
         <DiscordSignIn />
         <ThemeToggle />
       </div>
