84992656d20e66e6a8335162f248ac3d15977e83
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-11-27 18:35:05 -0800

    centering nav

--- components/header.tsx
+++ components/header.tsx
@@ -5,12 +5,12 @@ import DiscordSignIn from "./discord/discord-sign-in";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between p-6 lg:px-8 bg-background">
-      <MainLogo />
-      <nav>
+    <header className="flex p-6 lg:px-8 bg-background">
+      <MainLogo className="flex-1" />
+      <nav className="flex justify-center flex-1">
         <MainNavMenu />
       </nav>
-      <div className="flex gap-4">
+      <div className="flex justify-end flex-1 gap-4">
         <DiscordSignIn />
         <ThemeToggle />
       </div>
