f610c6d3a27e9f1b66445d0d6ac6d265da73e7e4
bryman360 <bryman360@yahoo.com>
2023-11-30 15:09:16 -0800

    Changing from http to https

--- components/baseurl.tsx
+++ components/baseurl.tsx
@@ -1,3 +1,3 @@
 const env = process.env.NODE_ENV
-export const BASE_URL = env == "development" ? process.env.NEXT_PUBLIC_API_URL : "http://" + process.env.VERCEL_URL;
+export const BASE_URL = env == "development" ? process.env.NEXT_PUBLIC_API_URL : "https://" + process.env.VERCEL_URL;
 
