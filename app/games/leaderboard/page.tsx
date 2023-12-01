4432d0c996e889a6cc446a87cd3dc046bd438e53
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-11-30 17:14:19 -0800

    testing

--- app/games/leaderboard/page.tsx
+++ app/games/leaderboard/page.tsx
@@ -5,17 +5,22 @@ import formatPlayerStats from "./leaderboardBody";
 import LeaderboardCategory from "./leaderboardCategory";
 import TeamSizeMenu from "./teamSizeSelection";
 import { BASE_URL } from "@/components/baseurl";
-import { teamSizeDefault, rowsDefault, sortOptionDefault, dirOptionDefault } from "./common";
+import {
+  teamSizeDefault,
+  rowsDefault,
+  sortOptionDefault,
+  dirOptionDefault,
+} from "./common";
 
 import React from "react";
 
 const leaderboard_categories = [
   // [Category, Query Abbreviation, Tailwind class names specific to that category]
-  ['ELO'      , 'e', ''],
-  ['W'        , 'w', ''],
-  ['L'        , 'l', ''],
-  ['W/L Ratio', 'r', ''],
-]
+  ["ELO", "e", ""],
+  ["W", "w", ""],
+  ["L", "l", ""],
+  ["W/L Ratio", "r", ""],
+];
 
 const getStats = async (searchParams: {
   page: number;
@@ -30,13 +35,33 @@ const getStats = async (searchParams: {
   const rows = searchParams?.rows;
   const teamSizeOption = searchParams?.teamSize;
   try {
-    console.log("FETCHING PLAYERS FROM: " + BASE_URL +
-      "/api/players/?page=" + String(page) +"&sort=" + sortOption + "&dir=" + dirOption + "&teamSize=" + teamSizeOption + "&rows=" + rows)
+    console.log(
+      "FETCHING PLAYERS FROM: " +
+        BASE_URL +
+        "/api/players/?page=" +
+        String(page) +
+        "&sort=" +
+        sortOption +
+        "&dir=" +
+        dirOption +
+        "&teamSize=" +
+        teamSizeOption +
+        "&rows=" +
+        rows
+    );
     const res = await fetch(
       BASE_URL +
-        "/api/players/?page=" + String(page) +"&sort=" + sortOption + "&dir=" + dirOption + "&teamSize=" + teamSizeOption + "&rows=" + rows,
+        "/api/players/?page=" +
+        String(page) +
+        "&sort=" +
+        sortOption +
+        "&dir=" +
+        dirOption +
+        "&teamSize=" +
+        teamSizeOption +
+        "&rows=" +
+        rows,
       {
-        method: 'GET',
         cache: "no-store",
       }
     );
@@ -54,50 +79,68 @@ const getStats = async (searchParams: {
 export default async function Leaderboard({
   searchParams,
 }: {
-  searchParams: { page: number; sort: string; dir: string, rows: string, teamSize: string };
+  searchParams: {
+    page: number;
+    sort: string;
+    dir: string;
+    rows: string;
+    teamSize: string;
+  };
 }) {
   const page = Math.max(searchParams.page || 1, 1);
   const sortOption = searchParams?.sort ? searchParams.sort : sortOptionDefault;
   const dirOption = searchParams?.dir ? searchParams.dir : dirOptionDefault;
   const descending = dirOption == "desc" ? true : false;
-  const rowsPerPage = searchParams?.rows ? Number(searchParams.rows) : rowsDefault;
-  const teamSizeOption = searchParams?.teamSize ? searchParams.teamSize : teamSizeDefault;
-  const playerStatsFetch = await getStats({
+  const rowsPerPage = searchParams?.rows
+    ? Number(searchParams.rows)
+    : rowsDefault;
+  const teamSizeOption = searchParams?.teamSize
+    ? searchParams.teamSize
+    : teamSizeDefault;
+  const playerStatsFetch = (await getStats({
     page: page,
     sort: sortOption,
     dir: dirOption,
     rows: rowsPerPage,
-    teamSize: teamSizeOption
-  }) || {players: []};
+    teamSize: teamSizeOption,
+  })) || { players: [] };
   const playerStatsQuery = playerStatsFetch.players;
   const playerCount = playerStatsFetch.playerCount;
-  const startingRankNumber = descending ? (page - 1) * rowsPerPage : playerCount - ((page - 1) * rowsPerPage);
-  const leaderboardBody = formatPlayerStats(playerStatsQuery, startingRankNumber, descending)
+  const startingRankNumber = descending
+    ? (page - 1) * rowsPerPage
+    : playerCount - (page - 1) * rowsPerPage;
+  const leaderboardBody = formatPlayerStats(
+    playerStatsQuery,
+    startingRankNumber,
+    descending
+  );
   return (
     <>
-      <h1 className="flex justify-center text-3xl font-bold">Leaderboard <TeamSizeMenu /></h1>
+      <h1 className="flex justify-center text-3xl font-bold">
+        Leaderboard <TeamSizeMenu />
+      </h1>
       <br />
       <div className="md:mx-[10%] md:h-[65vh] h-[55vh] overflow-y-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
           <thead className="sticky top-0 text-xs text-gray-700 bg-gray-300 dark:bg-slate-700 dark:text-gray-400">
-              <tr key="header">
-                    <th scope="col" className="px-6 py-3" key="Player ID">
-                      Player ID
-                    </th>
-                  {leaderboard_categories.map((category)=>
-                    <th scope="col" className="px-6 py-3" key={category[0]}>
-                      <LeaderboardCategory
-                      category={category[0]}
-                      sortingAbbreviation={category[1]}
-                      defaultCategory={leaderboard_categories[0][1] == category[1] ? true: false}
-                      />
-                    </th>
-                  )}
-              </tr>
+            <tr key="header">
+              <th scope="col" className="px-6 py-3" key="Player ID">
+                Player ID
+              </th>
+              {leaderboard_categories.map((category) => (
+                <th scope="col" className="px-6 py-3" key={category[0]}>
+                  <LeaderboardCategory
+                    category={category[0]}
+                    sortingAbbreviation={category[1]}
+                    defaultCategory={
+                      leaderboard_categories[0][1] == category[1] ? true : false
+                    }
+                  />
+                </th>
+              ))}
+            </tr>
           </thead>
-          <tbody>
-
-          </tbody>
+          <tbody></tbody>
           {leaderboardBody}
         </table>
       </div>
--- components/baseurl.tsx
+++ components/baseurl.tsx
@@ -1,3 +1,5 @@
-const env = process.env.NODE_ENV
-export const BASE_URL = env == "development" ? process.env.NEXT_PUBLIC_API_URL : "https://" + process.env.VERCEL_URL;
-
+const env = process.env.NODE_ENV;
+export const BASE_URL =
+  env == "development"
+    ? "http://localhost:3000"
+    : "https://" + process.env.VERCEL_URL;
