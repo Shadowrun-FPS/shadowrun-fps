cf864d842f275141fda70b38493b7fd5140ef70e
bryman360 <bryman360@yahoo.com>
2023-12-04 14:03:52 -0800

    Reverting base url

--- lib/baseurl.ts
+++ lib/baseurl.ts
@@ -2,5 +2,4 @@ const env = process.env.NODE_ENV;
 export const BASE_URL =
   env == "development"
     ? "http://localhost:3000"
-    : process.env.NEXT_PUBLIC_VERCEL_URL
-    ;
+    : "https://" + process.env.VERCEL_URL;
