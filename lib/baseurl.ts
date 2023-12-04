ca0030f2b6836279bbaaf04742be8856cae58750
bryman360 <bryman360@yahoo.com>
2023-12-04 12:55:38 -0800

    Trying a different BASE_URL for preview

--- lib/baseurl.ts
+++ lib/baseurl.ts
@@ -2,4 +2,5 @@ const env = process.env.NODE_ENV;
 export const BASE_URL =
   env == "development"
     ? "http://localhost:3000"
-    : "https://" + process.env.VERCEL_URL;
+    : "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
+    ;
