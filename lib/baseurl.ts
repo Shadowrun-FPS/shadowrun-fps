0cd3a32126bbc457cda84032ffef7b213dac8987
bryman360 <bryman360@yahoo.com>
2023-12-21 16:25:16 -0800

    Removing https from BASE_URL

--- lib/baseurl.ts
+++ lib/baseurl.ts
@@ -2,4 +2,4 @@ const env = process.env.NODE_ENV;
 export const BASE_URL =
   env == "development"
     ? "http://localhost:3000"
-    : "https://" + process.env.VERCEL_URL;
+    : process.env.VERCEL_URL;
