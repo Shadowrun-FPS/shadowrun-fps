d8a9579b14b59ab0d4b006241548ffcc3f2c8fd5
Johnathan <johnathancarlton@outlook.com>
2024-01-13 18:38:45 -0800

    installed speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { ThemeProvider } from "@/components/theme-provider";
 import NextAuthProvider from "./providers/next-auth-provider";
