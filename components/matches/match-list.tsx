0805d38fc1e92e5bd6d8ebec6f844181c7718779
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-01-21 16:41:04 -0800

    centering match list

--- components/matches/match-list.tsx
+++ components/matches/match-list.tsx
@@ -7,9 +7,9 @@ type MatchListProps = {
 
 export default function MatchList({ matches }: MatchListProps) {
   return (
-    <div className="flex flex-wrap gap-8">
+    <div className="flex flex-wrap justify-center gap-8">
       {matches?.map((match: Match) => {
-        return <MatchCard key={match.matchId} className="w-72" match={match} />;
+        return <MatchCard key={match.matchId} className="w-96" match={match} />;
       })}
     </div>
   );
