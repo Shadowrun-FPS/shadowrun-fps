b6f47dba2fe389208986f40b43694b35b8b0b78a
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-01-28 13:38:18 -0800

    finishing map results

--- app/matches/[matchId]/display-map-results.tsx
+++ app/matches/[matchId]/display-map-results.tsx
@@ -4,7 +4,7 @@ export function DisplayMapResults({ result }: { result: MapResults }) {
   return (
     <div className="prose dark:prose-invert">
       <h3>Map: {result.map}</h3>
-      <h3>Scored By: {result.scoredBy}</h3>
+      <p>Scored By: {result.scoredBy}</p>
       <div>
         Team 1: {result.scores.team1.team} {result.scores.team1.rounds} rounds.
       </div>
