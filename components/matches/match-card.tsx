2632893e258f9ae25958d056d8eb7f82d59c6cb9
Johnathan <johnathancarlton@outlook.com>
2024-02-01 21:23:17 -0800

    editing the carousel on map-card

--- components/matches/match-card.tsx
+++ components/matches/match-card.tsx
@@ -23,37 +23,43 @@ export default function MatchCard({ match, className }: MatchCardProps) {
   const isMatchFull = teamSize * 2 === players.length;
 
   return (
-    <Card key={matchId} className={className}>
-      <CardHeader className="prose dark:prose-invert">
-        <CardTitle className="my-2" title={title}>
-          {title}
-        </CardTitle>
-        <CardDescription>{gameType}</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="max-w-md mx-auto prose dark:prose-invert">
-          <h2>Team Size: {teamSize}</h2>
-          <div className="grid grid-cols-2 gap-x-4">
-            <h5 className="p-2">Team 1</h5>
-            <h5 className="p-2">Team 2</h5>
-            {players.map(async (player: Player) => (
-              <PlayerItem
-                key={player.discordId}
-                discordId={player.discordId}
-                matchTeamSize={teamSize}
-              />
-            ))}
-          </div>
-        </div>
-      </CardContent>
+    <Card
+      key={matchId}
+      className={`flex flex-col min-h-[535px] m-4 ${className}`}
+    >
+      <div className="flex flex-col h-full">
+        <CardHeader className="prose dark:prose-invert">
+          <CardTitle className="my-2" title={title}>
+            {title}
+          </CardTitle>
+          <CardDescription>{gameType}</CardDescription>
+        </CardHeader>
 
-      <CardFooter className="grid grid-cols-2 gap-4">
-        <Button className="col-span-2">
-          <Link href={`/matches/${matchId}`}>View Match</Link>
-        </Button>
-        <JoinButton matchId={matchId} isMatchFull={isMatchFull} />
-        <LeaveButton matchId={matchId} players={players} />
-      </CardFooter>
+        <CardContent className="flex-grow">
+          <div className="max-w-md mx-auto prose dark:prose-invert">
+            <h2>Team Size: {teamSize}</h2>
+            <div className="grid grid-cols-2 gap-x-4">
+              <h5 className="p-2">Team 1</h5>
+              <h5 className="p-2">Team 2</h5>
+              {players.map(async (player: Player) => (
+                <PlayerItem
+                  key={player.discordId}
+                  discordId={player.discordId}
+                  matchTeamSize={teamSize}
+                />
+              ))}
+            </div>
+          </div>
+        </CardContent>
+
+        <CardFooter className="grid grid-cols-2 gap-4 mt-auto">
+          <Button className="col-span-2">
+            <Link href={`/matches/${matchId}`}>View Match</Link>
+          </Button>
+          <JoinButton matchId={matchId} isMatchFull={isMatchFull} />
+          <LeaveButton matchId={matchId} players={players} />
+        </CardFooter>
+      </div>
     </Card>
   );
 }
