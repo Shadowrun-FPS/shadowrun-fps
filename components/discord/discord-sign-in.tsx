501d27ba2b837cfd007386bd221dcd314cec9818
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-02-04 17:46:30 -0800

    added spiner while login is loading

--- components/discord/discord-sign-in.tsx
+++ components/discord/discord-sign-in.tsx
@@ -1,15 +1,15 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Spinner from "@/components/util/spinner";
+
 import IconDiscordLogo from "../icons/discord-logo";
 import { signIn, signOut } from "next-auth/react";
 
 import { useSession } from "next-auth/react";
 
 const DiscordSignIn: React.FC = () => {
-  const { status, data } = useSession();
-  console.log({ data });
-  const isSignedIn = status === "authenticated";
+  const { status } = useSession();
 
   const handleSignIn = () => {
     console.log("Discord sign in");
@@ -21,18 +21,31 @@ const DiscordSignIn: React.FC = () => {
     signOut();
   };
 
-  return (
-    <Button
-      className={`gap-2 min-w-fit ${isSignedIn ? "bg-red-500" : "bg-discord"}`}
-      variant="outline"
-      onClick={isSignedIn ? handleSignOut : handleSignIn}
-    >
-      <IconDiscordLogo height={"2em"} width={"2em"} />
-      <span className="hidden md:block">
-        {isSignedIn ? "Sign out" : "Sign in"}
-      </span>
-    </Button>
-  );
+  if (status === "loading") {
+    return <Spinner />;
+  } else if (status === "authenticated") {
+    return (
+      <Button
+        className={"gap-2 min-w-fit bg-red-500"}
+        variant="outline"
+        onClick={handleSignOut}
+      >
+        <IconDiscordLogo height={"2em"} width={"2em"} />
+        <span className="hidden md:block">Sign out</span>
+      </Button>
+    );
+  } else {
+    return (
+      <Button
+        className={"gap-2 min-w-fit bg-discord"}
+        variant="outline"
+        onClick={handleSignIn}
+      >
+        <IconDiscordLogo height={"2em"} width={"2em"} />
+        <span className="hidden md:block">Sign in</span>
+      </Button>
+    );
+  }
 };
 
 export default DiscordSignIn;
