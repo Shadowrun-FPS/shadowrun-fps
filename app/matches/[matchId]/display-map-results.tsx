09c6420386e7d140f2d643f0dc86456b247ccb44
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-02-11 20:44:58 -0800

    updated carousel

--- app/matches/[matchId]/display-map-results.tsx
+++ app/matches/[matchId]/display-map-results.tsx
@@ -1,17 +1,16 @@
 import { MapResults } from "@/types/types";
 
 export function DisplayMapResults({ result }: { result: MapResults }) {
-  // TODO: Map results example: "Team 1 (6-4)" for team one winning with a score of 6
+  const winner =
+    result.scores.team1.rounds > result.scores.team2.rounds
+      ? "Team 1"
+      : "Team 2";
   return (
     <div className="prose dark:prose-invert">
-      <h3>Map: {result.map}</h3>
-      <p>Scored By: {result.scoredBy}</p>
-      <div>
-        Team 1: {result.scores.team1.team} {result.scores.team1.rounds} rounds.
-      </div>
-      <div>
-        Team 2: {result.scores.team2.team} {result.scores.team2.rounds} rounds.
-      </div>
+      <p>
+        {winner} wins {result.scores.team1.rounds} to
+        {result.scores.team2.rounds}
+      </p>
     </div>
   );
 }
