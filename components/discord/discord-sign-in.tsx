7ee1911bc98690a8a5ac9e24969f9f3c43e74b9d
Johnathan <johnathancarlton@outlook.com>
2024-04-25 23:16:08 -0700

    added discord avatar beside discord sign out

--- components/discord/discord-sign-in.tsx
+++ components/discord/discord-sign-in.tsx
@@ -5,11 +5,12 @@ import Spinner from "@/components/util/spinner";
 
 import IconDiscordLogo from "../icons/discord-logo";
 import { signIn, signOut } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import { useSession } from "next-auth/react";
 
 const DiscordSignIn: React.FC = () => {
-  const { status } = useSession();
+  const { data: session, status } = useSession();
 
   const handleSignIn = () => {
     console.log("Discord sign in");
@@ -21,18 +22,31 @@ const DiscordSignIn: React.FC = () => {
     signOut();
   };
 
+  let avatarSrc = "";
   if (status === "loading") {
     return <Spinner />;
-  } else if (status === "authenticated") {
+  } else if (status === "authenticated" && session) {
+    // Check if user data exists in session
+    const { user } = session;
+    if (user && user.image) {
+      // If user has a profile image, use it as the avatar source
+      avatarSrc = user.image;
+    }
     return (
-      <Button
-        className={"gap-2 min-w-fit bg-red-500"}
-        variant="outline"
-        onClick={handleSignOut}
-      >
-        <IconDiscordLogo height={"2em"} width={"2em"} />
-        <span className="hidden md:block">Sign out</span>
-      </Button>
+      <>
+        <Avatar>
+          <AvatarImage src={avatarSrc} />
+          <AvatarFallback>{user.name}</AvatarFallback>
+        </Avatar>
+        <Button
+          className={"gap-2 min-w-fit bg-red-500"}
+          variant="outline"
+          onClick={handleSignOut}
+        >
+          <IconDiscordLogo height={"2em"} width={"2em"} />
+          <span className="hidden md:block">Sign out</span>
+        </Button>
+      </>
     );
   } else {
     return (
